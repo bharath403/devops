1cc35ecbba1e3ca27aa00432d3d60d53d6cae493
Poosa <bpoosa691@hqsml-145392.cable.comcast.com>
2018-06-28 15:22:19 -0400

    comment

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "aws_subnet" {
 variable "aws_amis" {
   default = {
     eu-west-1 = "ami-674cbc1e"
-    us-east-1 = "ami-1d4e7a66"
+#    us-east-1 = "ami-1d4e7a66"
     us-west-1 = "ami-969ab1f6"
     us-west-2 = "ami-8803e0f0"
   }
