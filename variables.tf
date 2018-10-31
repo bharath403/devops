9d780b6c73a35632be741b55c8ab0a953437cb04
Bharath Poosa <Bharath_Poosa2@comcast.com>
2018-10-31 14:44:08 -0400

    full path

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ Ensure this keypair is added to your local SSH agent so provisioners can
 connect.
 Example: ~/.ssh/terraform.pub
 DESCRIPTION
-  default = ~/.ssh/terraform.pub
+  default = /Users/bpoosa691/.ssh/terraform.pub
 }
 
 variable "key_name" {
